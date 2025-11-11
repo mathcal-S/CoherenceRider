9704ef5c0725efb370f820ab5b762f7750db63f8
MARCO ANTONIO ROCHA.md <mathcal112358pi@outlook.com>
2025-11-11 08:12:07 -0700

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,7 @@
-
-```javascript
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['expo-router/babel', 'react-native-reanimated/plugin'],
+    plugins: ['react-native-reanimated/plugin'],
   };
 };
-```
